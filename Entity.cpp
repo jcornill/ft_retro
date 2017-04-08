44fe6052b5070b2a4bddc6876545824d2df542e1
Arnaud MATHIAS <amathias@e2r7p20.42.fr>
2017-04-08 12:40:05 +0200

    removed entity size

--- Entity.cpp
+++ Entity.cpp
@@ -2,12 +2,11 @@
 #include "Display.hpp"
 #include "Logger.hpp"
 
-Entity::Entity(void) : _posX(1), _posY(1), _sizeX(0), _sizeY(0), _hasPosChanged(false), _drawingChar('>') {
+Entity::Entity(void) : _posX(1), _posY(1), _hasPosChanged(false), _drawingChar('>') {
 	//Logger::LogToFile("Creating Entity");
 }
 
-Entity::Entity(int posX, int posY, int sizeX, int sizeY, bool hasChanged) : _posX(posX), _posY(posY),
-										_sizeX(sizeX), _sizeY(sizeY), _hasPosChanged(hasChanged){
+Entity::Entity(int posX, int posY, int sizeX, int sizeY, bool hasChanged) : _posX(posX), _posY(posY), _hasPosChanged(hasChanged){
 
 }
 
@@ -23,8 +22,6 @@ Entity &	Entity::operator=(Entity const & rhs) {
 	if (this != &rhs){
 		this->_posX = rhs._posX;
 		this->_posY = rhs._posY;
-		this->_sizeX = rhs._sizeX;
-		this->_sizeY = rhs._sizeY;
 	}
 	return (*this);
 }
