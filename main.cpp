c38db9c781a4b4832138227eca8d394e5ddce86b
Arnaud MATHIAS <amathias@e2r7p20.42.fr>
2017-04-09 16:24:01 +0200

    Autosaving rush

--- main.cpp
+++ main.cpp
@@ -7,17 +7,39 @@ int main() {
 	Display *nCurse = new Display(210, 42);
 	if (nCurse->IsInit())
 	{
-		Game::Instance->GameLoop();
-		delete Game::Instance;
-		delete nCurse;
-//		nCurse = new Display(355, 78);
-		Game::Instance = new Game();
-		Game::Instance->GameLoop();
+		bool relaunch = true;
+		while (relaunch) {
+			Game::Instance->GameLoop();
+			delete Game::Instance;
+			Game::Instance = 0;
+			delete nCurse;
+
+			nCurse = new Display(210, 42);
+			Display::PutStr("G A M E   O V E R ", (Display::sizeX / 2) - 7, Display::sizeY / 2);
+			Display::PutStr("Press r to relaunch", (Display::sizeX / 2) - 7, (Display::sizeY / 2) + 2);
+			Display::PutStr("Press q to quit", (Display::sizeX / 2) - 7, (Display::sizeY / 2) + 3);
+
+			Display::Refresh();
+			bool isRunning = true;
+			while (isRunning) {
+				int key = Display::GetKey();
+				if (key == 'q') {
+					isRunning = false;
+					relaunch = false;
+				}
+				if (key == 'r') {
+					isRunning = false;
+					Game::Instance = new Game();
+					delete nCurse;
+					nCurse = new Display(210, 42);
+				}
+			}
+		}
 	}
 	else
 	{
 		Logger::LogToFile("Error when loading ncurses !");
 	}
-	delete Game::Instance;
+	//delete Game::Instance;
 	delete nCurse;
 }
